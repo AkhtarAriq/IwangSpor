fe24fae1e2764bd279afa57ae79fb2de9158fe85
striarmdhan <@muhhamadsatriaramadhan1@gmail.com>
2024-04-07 21:29:54 +0700

    pushCode

---
+++ client/src/components/dashboard/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const dashboard = () => {
+    return(
+        <div>
+            This is Dashboard Page
+            <a href="/">Log out</a>
+        </div>
+    )
+}
+
+export default dashboard
